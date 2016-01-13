e872ffe3ac57e8c65399700350073dd3eb3fae31
jrfnl <github_nospam@adviesenzo.nl>
2016-01-13 02:30:14 +0100

    Up version nrs for release.

--- class-debug-bar-list-php-classes.php
+++ class-debug-bar-list-php-classes.php
@@ -9,9 +9,9 @@
  * @package    Debug Bar Pretty Output
  * @author     Juliette Reinders Folmer <wpplugins_nospam@adviesenzo.nl>
  * @link       https://github.com/jrfnl/debug-bar-pretty-output
- * @version    1.5
+ * @version    1.5.2
  *
- * @copyright  2013 Juliette Reinders Folmer
+ * @copyright  2013-2016 Juliette Reinders Folmer
  * @license    http://creativecommons.org/licenses/GPL/2.0/ GNU General Public License, version 2 or higher.
  */
 
